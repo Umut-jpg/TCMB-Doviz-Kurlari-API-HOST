5363ac7778ebdcbe27036cdbae9c671f9a78c7de
Umut-jpg <yusaumutyerlicay@gmail.com>
2023-09-08 13:56:02 +0300

    dad

--- v/main.py
+++ v/main.py
@@ -16,7 +16,23 @@ current_dateTime = datetime.now()
 @app.route('/api/get_data', methods=['GET'])
 def get_data():
     evds = evdsAPI('WRQmTwGYmQ')
-    data = evds.get_data(['TP.DK.USD.A.YTL','TP.DK.EUR.A.YTL'], startdate=f"{current_dateTime.day}-{current_dateTime.month}-{current_dateTime.year}" ,enddate=f"{current_dateTime.day}-{current_dateTime.month}-{current_dateTime.year},raw=True")
+    data = evds.get_data([
+        'TP.DK.USD.A.YTL','TP.DK.USD.S.YTL',
+        'TP.DK.EUR.A.YTL','TP.DK.EUR.S.YTL',
+        'TP.DK.GBP.A.YTL','TP.DK.GBP.S.YTL',
+        'TP.DK.CHF.A.YTL','TP.DK.CHF.S.YTL',
+        'TP.DK.AUD.A.YTL','TP.DK.AUD.S.YTL',
+        'TP.DK.CAD.A.YTL','TP.DK.CAD.S.YTL',
+        'TP.DK.SAR.A.YTL','TP.DK.SAR.S.YTL',
+        'TP.DK.AED.A.YTL','TP.DK.AED.S.YTL',
+        'TP.DK.QAR.A.YTL','TP.DK.QAR.S.YTL',
+        'TP.DK.KWD.A.YTL','TP.DK.KWD.S.YTL',
+        'TP.DK.JPY.A.YTL','TP.DK.JPY.S.YTL',
+        'TP.DK.NOK.A.YTL','TP.DK.NOK.S.YTL',
+        'TP.DK.SEK.A.YTL','TP.DK.SEK.S.YTL',
+        'TP.DK.AZN.A.YTL','TP.DK.AZN.S.YTL',
+        'TP.DK.KRW.A.YTL','TP.DK.KRW.S.YTL',
+        ], startdate=f"{current_dateTime.day}-{current_dateTime.month}-{current_dateTime.year}" ,enddate=f"{current_dateTime.day}-{current_dateTime.month}-{current_dateTime.year},raw=True")
     #json_data = data.to_json(orient='records') 
     data_list = data.values.tolist()
     return jsonify(data_list)
