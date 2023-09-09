0f7f296177581945b56b927a2ddc4c10590255c1
Umut-jpg <yusaumutyerlicay@gmail.com>
2023-09-09 17:54:57 +0300

    dad

--- v/main.py
+++ v/main.py
@@ -1,21 +1,30 @@
-# from evds import evdsAPI
-# evds = evdsAPI('WRQmTwGYmQ')
-# deneme= evds.get_data(['TP.DK.USD.A.YTL','TP.DK.EUR.A.YTL'], startdate="07-09-2023", enddate="07-09-2023")
-# print(deneme)
+
 
 from flask import Flask, jsonify
 from evds import evdsAPI
-from flask_cors import CORS 
-from datetime  import datetime
+from flask_cors import CORS
+from datetime import datetime, timedelta
+
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
+evds = evdsAPI('WRQmTwGYmQ')
+
 @app.route('/')
 def home():
     return "404 Not Found"
-current_dateTime = datetime.now()
-@app.route('/api/get_data', methods=['GET'])
-def get_data():
-    evds = evdsAPI('WRQmTwGYmQ')
+
+def get_last_weekday_data():
+    current_dateTime = datetime.now()
+    print(current_dateTime.weekday())
+   
+    if current_dateTime.weekday() == 5:
+        current_dateTime -= timedelta(days=1)
+         
+    
+    elif current_dateTime.weekday() == 6:
+        current_dateTime -= timedelta(days=2)
+
+ 
     data = evds.get_data([
         'TP.DK.USD.A.YTL','TP.DK.USD.S.YTL',
         'TP.DK.EUR.A.YTL','TP.DK.EUR.S.YTL',
@@ -32,12 +41,66 @@ def get_data():
         'TP.DK.SEK.A.YTL','TP.DK.SEK.S.YTL',
         'TP.DK.AZN.A.YTL','TP.DK.AZN.S.YTL',
         'TP.DK.KRW.A.YTL','TP.DK.KRW.S.YTL',
-        ], startdate=f"{current_dateTime.day}-{current_dateTime.month}-{current_dateTime.year}" ,enddate=f"{current_dateTime.day}-{current_dateTime.month}-{current_dateTime.year},raw=True")
-    #json_data = data.to_json(orient='records') 
-    data_list = data.values.tolist()
-    return jsonify(data_list)
+        
+    ],startdate=f"{current_dateTime.day}-{current_dateTime.month}-{current_dateTime.year}", enddate=f"{current_dateTime.day}-{current_dateTime.month}-{current_dateTime.year}", raw=True)
+
+    return data
+ 
+@app.route('/api/get_data', methods=['GET'])
+def get_data():
+    data = get_last_weekday_data()
+
+    if not data:
+        return "Veri bulunamadı."
+
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
+
+
+# # from evds import evdsAPI
+# # evds = evdsAPI('WRQmTwGYmQ')
+# # deneme= evds.get_data(['TP.DK.USD.A.YTL','TP.DK.EUR.A.YTL'], startdate="07-09-2023", enddate="07-09-2023")
+# # print(deneme)
+
+# from flask import Flask, jsonify
+# from evds import evdsAPI
+# from flask_cors import CORS 
+# from datetime  import datetime
+# app = Flask(__name__)
+# CORS(app) 
+# @app.route('/')
+# def home():
+#     return "404 Not Found"
+# current_dateTime = datetime.now()
+# @app.route('/api/get_data', methods=['GET'])
+# def get_data():
+#     evds = evdsAPI('WRQmTwGYmQ')
+#     data = evds.get_data([
+#         'TP.DK.USD.A.YTL','TP.DK.USD.S.YTL',
+#         'TP.DK.EUR.A.YTL','TP.DK.EUR.S.YTL',
+#         'TP.DK.GBP.A.YTL','TP.DK.GBP.S.YTL',
+#         'TP.DK.CHF.A.YTL','TP.DK.CHF.S.YTL',
+#         'TP.DK.AUD.A.YTL','TP.DK.AUD.S.YTL',
+#         'TP.DK.CAD.A.YTL','TP.DK.CAD.S.YTL',
+#         'TP.DK.SAR.A.YTL','TP.DK.SAR.S.YTL',
+#         'TP.DK.AED.A.YTL','TP.DK.AED.S.YTL',
+#         'TP.DK.QAR.A.YTL','TP.DK.QAR.S.YTL',
+#         'TP.DK.KWD.A.YTL','TP.DK.KWD.S.YTL',
+#         'TP.DK.JPY.A.YTL','TP.DK.JPY.S.YTL',
+#         'TP.DK.NOK.A.YTL','TP.DK.NOK.S.YTL',
+#         'TP.DK.SEK.A.YTL','TP.DK.SEK.S.YTL',
+#         'TP.DK.AZN.A.YTL','TP.DK.AZN.S.YTL',
+#         'TP.DK.KRW.A.YTL','TP.DK.KRW.S.YTL',
+#         ], startdate=f"{current_dateTime.day}-{current_dateTime.month}-{current_dateTime.year}" ,enddate=f"{current_dateTime.day}-{current_dateTime.month}-{current_dateTime.year},raw=True")
+#     #json_data = data.to_json(orient='records') 
+#     data_list = data.values.tolist()
+#     return jsonify(data_list)
 
 
  
-if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+# if __name__ == '__main__':
+#     app.run(host='localhost', port=5000, debug=True)
+
+
